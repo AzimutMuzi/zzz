a46868ed13b211684021d3a6a31c94d664a567d1
AzimutMuzi <164947970+AzimutMuzi@users.noreply.github.com>
2024-03-29 13:55:20 +0300

    Add files via upload

---
+++ mehaniz.js
@@ -0,0 +1,31 @@
+function est() {
+const pervoePhoto=
+document.querySelector("img");
+pervoePhoto.src="photo/est.jpg";
+pervoePhoto.style.backgroundColor="#c60018";
+}
+function krasota() {
+const vtoroePhoto=
+document.querySelector("img");
+vtoroePhoto.src="photo/kosmetik.jpg";
+vtoroePhoto.style.backgroundColor="#c66b63";
+}
+function uborka() {
+const tretiePhoto=
+document.querySelector("img");
+tretiePhoto.src="photo/uborka.jpg";
+tretiePhoto.style.backgroundColor="#c6ad84";
+}
+function umat() {
+const chetPhoto=
+document.querySelector("img");
+chetPhoto.src="photo/dumat.jpg";
+chetPhoto.style.backgroundColor="#74c6bef1";
+}
+function luv() {
+const pyatPhoto=
+document.querySelector("img");
+pyatPhoto.src="photo/vlublena.jpg";
+pyatPhoto.style.backgroundColor="#538939f1";
+}
+
